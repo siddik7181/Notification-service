2225645fd584d6528ed0bf95bd404221975e45d4
Nahid <Abu.Siddik@optimizely.com>
2025-01-15 12:39:02 +0600

    should fix the endpoint error for k6 stress test

--- stress_test.js
+++ stress_test.js
@@ -1,13 +1,11 @@
-// Stress testing assesses how the system performs when loads are heavier than usual.
 
 import http from "k6/http";
 import { check, sleep } from "k6";
 
 export const options = {
-  // Key configurations for Stress in this section
   stages: [
-    { duration: "10s", target: 200 }, // traffic ramp-up from 1 to a higher 200 users over 10 minutes.
-    { duration: "1m", target: 200 }, // stay at higher 200 users for 30 minutes
+    { duration: "30s", target: 100 }, // traffic ramp-up from 1 to a higher 200 users over 30 sec.
+    { duration: "2m", target: 100 }, // stay at higher 200 users for 2 minute
     { duration: "10s", target: 0 }, // ramp-down to 0 users
   ],
 };
@@ -20,17 +18,17 @@ export default function () {
 
   check(mailRes, {
     "is status 200": (r) => r.status === 200,
-    "mailRes time is acceptable": (r) => r.timings.duration < 2000, // example threshold
+    "mailRes time is acceptable": (r) => r.timings.duration < 3000,
   });
   check(smsRes, {
     "is status 200": (r) => r.status === 200,
-    "smsRes time is acceptable": (r) => r.timings.duration < 2000, // example threshold
+    "smsRes time is acceptable": (r) => r.timings.duration < 3000,
   });
 }
 
 const test_data = {
-  EMAIL_URL: "http://notify-server:8080/send/mail",
-  SMS_URL: "http://notify-server:8080/send/sms",
+  EMAIL_URL: "http://notify-server:8080/api/email",
+  SMS_URL: "http://notify-server:8080/api/sms",
 
   mailPayload: JSON.stringify({
     subject: "A subject has nothing🙄!",
